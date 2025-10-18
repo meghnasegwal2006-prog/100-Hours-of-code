f85234bebe3b397bcab20b299f88238c3d80a2db
meghnasegwal2006-prog <meghnasegwal2006@gmail.com>
2025-10-18 19:05:15 +0530

    Add files via upload

---
+++ 10.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ int main() {
+    float a, b, c;
+    printf("Enter three sides of the triangles: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == b && b == c)
+       printf("Traingle is Equilateral.\n");
+    else if (a == b || b == c || a == c)
+          printf("Triangle is Isosceles.\n");
+    else
+         printf("Triangle is Scalene.\n");
+    
+    return 0;      
+    
+ }
